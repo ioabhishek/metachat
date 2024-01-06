6503aeb96225fb80382cd91ed0949acb461f3626
Abhishek Sharma <akstenx@gmail.com>
2024-01-06 22:54:18 +0530

    latest changes

--- src/components/ui/SubscribeLeaveToggle.tsx
+++ src/components/ui/SubscribeLeaveToggle.tsx
@@ -1,11 +1,16 @@
 "use client"
 import { FC } from "react"
 import { Button } from "./Button"
+import { useMutation } from "@tanstack/react-query"
 
 interface SubscribeLeaveToggleProps {}
 
 const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({}) => {
   const isSubscribed = false
+
+  const {} = useMutation({
+    mutationFn: async () => {},
+  })
 
   return isSubscribed ? (
     <Button className="w-full mt-1 mb-4">Leave Community</Button>
